e8060e52397aa6b213afce8fb0089f161efad9d9
EvgeniaMIL <e.vaylet@mail.ru>
2022-08-10 12:26:34 +0300

    init:finish work

--- src/index.js
+++ src/index.js
@@ -37,10 +37,36 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+    function decode(expr) {
+        let arrSplit = expr.split('');
+        let arr = [];
+        let result = '';
 
-module.exports = {
-    decode
-}
+    
+        while (arrSplit.length > 0) {
+            arr.push(arrSplit.splice(0, 10));
+        }
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].includes('*')) {
+                result += ' ';
+                continue;
+            }
+
+
+        let str = '';
+        for (let j = 0; j < arr[i].length; j += 2) {
+            if (arr[i][j] === '1' && arr[i][j + 1] === '0') str += '.';
+            else if (arr[i][j] === '1' && arr[i][j + 1] === '1') str += '-';
+        }
+        result += MORSE_TABLE[str];
+    
+        }
+
+        return result;
+    }
+
+
+    module.exports = {
+        decode
+    }
